05d4973ef12a24b9ddd2c0d669bbfe10bf3c52d5
spacecop <copiosacumlucis@gmail.com>
2022-07-30 06:54:49 -0700

    Task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,8 +7,7 @@ def fizzbuzz():
                     if i % 3 == 0 and i % 5 == 0:
 
                                     print("FizzBuzz ", end="")
-
-                                            elif i % 3 == 0:
+                                elif i % 3 == 0:
 
                                                             print("Fizz ", end="")
 
