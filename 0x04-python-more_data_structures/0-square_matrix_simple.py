7af9aaa766c842dba1d6848a4a34e2453028c34d
spacecop <copiosacumlucis@gmail.com>
2022-08-24 19:13:46 -0700

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """
-    Computes the square value of all integers of a matirx
-    ...
-
-    Parameters
-    -----------
-    matirx : list (of lists)
-    The list of elements
-
-    Return:
-    a new matrix:
-    Same size as matrix
-    Each value should be the square of the value of the input
-    """
-
-    return ([list(map(lambda x: x * x, row)) for row in matirx])
-
+    if not matrix:
+        print()
+        return [[item**2 for item in row] for row in matrix]
