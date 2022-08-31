088c0aed6dd8cdf8f340e137f2f3c62ca4f947fe
spacecop <copiosacumlucis@gmail.com>
2022-08-30 20:53:27 -0700

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [find if find != search else replace for find in my_list]
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+            return (new_list)
